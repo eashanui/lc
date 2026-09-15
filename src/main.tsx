8a7552cdaca89ad3ac348df59f8c18be8fff5092
eashancode <eashan@geveo.com>
2026-09-15 09:46:56 +0530

    Fix GitHub Pages deploy artifact path

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { RouterProvider, createHashHistory, createRouter } from "@tanstack/react-router";
 
 import "./styles.css";
@@ -13,8 +13,8 @@ const router = createRouter({
   },
 });
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </StrictMode>,
 );
